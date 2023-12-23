79890f74b0c12d5a010e502dfef9e39bc06c8aac
SoohoLee <hyeri0603@naver.com>
2023-12-23 12:39:19 +0900

    코틀린 테스트 231222

---
+++ src/main/kotlin/typeDifference/inJava.java
@@ -0,0 +1,16 @@
+package typeDifference;
+
+import domain.Person;
+
+public class inJava {
+
+    public static void main(String[] args) {
+    }
+
+    public static void printAgeIfPerson(Object obj) {
+        if (obj instanceof Person) {
+            Person person = (Person) obj;
+            System.out.println(person.getAge());
+        }
+    }
+}
---
+++ src/main/kotlin/variableDifference/inKotlin.kt
@@ -0,0 +1,18 @@
+package org.example.variableDifference
+
+fun main() {
+//    var number1 = 10L
+    var number1: Long = 10L
+    number1 = 5L
+
+//    val number2 = 10L
+    val number2: Long = 10L
+//    number2 = 5L  불가능
+
+    println(number1)
+
+    var number3: Long? = 1_000L
+    number3 = null
+
+//    var person = Person("이수호")
+}
